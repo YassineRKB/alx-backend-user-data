be893219b44f9351e505e0374c19ebc463d9c0de
YassineRKB <yassine@arcraven.com>
2024-02-09 01:12:49 +0100

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""module for filtered_logger"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str],
+                 redaction: str, message: str,
+                 separator: str) -> str:
+    """func for filtering a message"""
+    for Afield in fields:
+        message = re.sub(f'{Afield}=.*?{separator}',
+                         f'{Afield}={redaction}{separator}',
+                         message)
+    return message
