ccc5ac3af4c23e604f17304fb84197be0e711a02
YassineRKB <yassine@arcraven.com>
2024-02-09 02:02:04 +0100

    task 3 typo?

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """module for filtered_logger"""
-from typing import List
 import re
-import os
+from typing import List
 import logging
+import os
 import mysql.connector
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
