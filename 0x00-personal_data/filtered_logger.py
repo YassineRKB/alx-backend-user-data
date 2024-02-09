95f76dd49771d6296bdd98f829c3e14568faf396
YassineRKB <yassine@arcraven.com>
2024-02-09 01:17:09 +0100

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """module for filtered_logger"""
 from typing import List
-import re
+import re, logging
 
 
 def filter_datum(fields: List[str],
@@ -13,3 +13,20 @@ def filter_datum(fields: List[str],
                          f'{Afield}={redaction}{separator}',
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.message = filter_datum(self.fields, self.REDACTION,
+                                  record.message, self.SEPARATOR)
+        res = super().format(record)
+        return res
