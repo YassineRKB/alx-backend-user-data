a85d7a56f37b2456e8000f60d2a7cc372cbd25e3
YassineRKB <yassine@arcraven.com>
2024-02-09 02:11:37 +0100

    task 5

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""module for passwd encryption"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """hashes a password"""
+    salt = bcrypt.gensalt()
+    encpasswd = password.encode('utf-8')
+    hash = bcrypt.hashpw(encpasswd, salt)
+    return hash
