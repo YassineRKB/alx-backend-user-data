f8391704959a4981a9c579586d8e13412d6a4ed7
YassineRKB <yassine@arcraven.com>
2024-02-14 03:29:52 +0100

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """module for basic authentication"""
 from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
+import base64
 
 
 class BasicAuth(Auth):
@@ -36,3 +39,15 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':')
         return email, password
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Retrieves a user object based on provided email and password."""
+        if not user_email or not user_pwd \
+                or type(user_email) != str or type(user_pwd) != str:
+            return None
+        user = User.search({'email': user_email})
+        if not user or not user[0].is_valid_password(user_pwd):
+            return None
+        return user[0]
