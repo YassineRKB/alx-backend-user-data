e35e2e3b433968cd6d70df9f36751846c02712e9
YassineRKB <yassine@arcraven.com>
2024-02-14 02:39:35 +0100

    task 6

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""module for basic authentication"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """class for basic authentication"""
+
+    pass
