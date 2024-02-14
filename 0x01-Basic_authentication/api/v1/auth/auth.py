11f8963e743b2d05f8ec0ccd763202d9ec8dbaad
YassineRKB <yassine@arcraven.com>
2024-02-14 02:34:42 +0100

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,7 +18,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Authorization header."""
-        return None
+        if not request:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar("User"):
         """Current user."""
