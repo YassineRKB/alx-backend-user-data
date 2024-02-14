85aceb3c597aa7887907d362afeb9003b801b552
YassineRKB <yassine@arcraven.com>
2024-02-14 03:27:14 +0100

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,3 +14,15 @@ class BasicAuth(Auth):
                 or not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes the Base64-encoded part of an Authorization header."""
+        if not base64_authorization_header \
+                or type(base64_authorization_header) != str:
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header.encode())\
+                    .decode('utf-8')
+        except base64.binascii.Error:
+            return None
