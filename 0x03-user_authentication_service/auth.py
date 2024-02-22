61b8a21260e0ffd3e9bf43a82bc971a66b6d4633
YassineRKB <yassine@arcraven.com>
2024-02-22 16:21:48 +0100

    auth datatype fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,7 +75,7 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
 
-    def update_password(self, reset_token: str, password: str):
+    def update_password(self, reset_token: str, password: str) -> None:
         """password update"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
