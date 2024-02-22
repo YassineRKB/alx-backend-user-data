f1bfb416b6707f9ea68dba53dc5a58c5df9be825
YassineRKB <yassine@arcraven.com>
2024-02-22 15:36:09 +0100

    users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 host = "0.0.0.0"
 port = "5000"
 
@@ -12,6 +16,20 @@ def welcome():
     """home route"""
     data = {"message": "Bienvenue"}
     return jsonify(data)
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """users route"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    msgOk = {"email": email, "message": "user created"}
+    msgErr = {"message": "email already registered"}
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify(msgErr), 400
+    return jsonify(msgOk)
 
 
 if __name__ == "__main__":
