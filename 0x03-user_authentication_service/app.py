44a4b4685402c99c70b6cfdb2595df50e39b352f
YassineRKB <yassine@arcraven.com>
2024-02-22 15:31:33 +0100

    basic app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""flask app"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+host = "0.0.0.0"
+port = "5000"
+
+
+@app.route("/")
+def welcome():
+    """home route"""
+    data = {"message": "Bienvenue"}
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(
+        host=host,
+        port=port
+    )
